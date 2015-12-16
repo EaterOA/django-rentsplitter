9197fd295e8500fca81f42cd62c8b6984671c8dc
Vincent Wong <duperduper@ucla.edu>
2015-12-16 00:51:05 -0800

    Modify Entry to have a date field

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
     name    = models.CharField(max_length=100, unique=True)
@@ -12,6 +13,7 @@ class Entry(models.Model):
     amount  = models.DecimalField(max_digits=20, decimal_places=2)
     payer   = models.ForeignKey(User, on_delete=models.CASCADE)
     note    = models.CharField(max_length=4000)
+    date    = models.DateTimeField(default=timezone.now)
 
 class DebtEntry(Entry):
     payee   = models.ForeignKey(User, on_delete=models.CASCADE)
